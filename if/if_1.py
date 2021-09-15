1dabdad6fe5be433474a871438fc35f52de2f8ca
dew <pjj3412@gmail.com>
2021-09-16 02:51:56 +0900

    if

---
+++ if/if_1.py
@@ -0,0 +1,8 @@
+# if문
+print("수를 입력하세요: ")
+a = int(input())
+
+if a == 0:
+  print("0은 나눗셈에 이용할 수 없습니다.")
+else:
+  print("3 /", a, '=', 3/a)
---
+++ if/if_2.py
@@ -0,0 +1,8 @@
+# if elif else문
+money = 10000
+if money > 5000:
+  print("택시 타고 가자.")
+elif money > 1000:
+  print("버스 타고 가자.")
+else:
+  print("걸어가자.")
